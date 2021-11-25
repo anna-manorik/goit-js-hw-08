5ef4ad2d4c6f860ac945b0189d1f440dcca80468
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2021-11-26 00:09:03 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
 
+import VimeoPlayer from "@vimeo/player";
+
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+player.on('timeupdate', _.throttle(run, 1000));
+
+function run(data) {
+    const currTime = data.seconds;
+    console.log(currTime);
+
+    localStorage.setItem("videoplayer-current-time", currTime);
+}
+
+player.on('play', function() {
+    const currTime = localStorage.getItem("videoplayer-current-time");
+
+    if(currTime !== null) {
+        player.setCurrentTime(currTime);
+    }
+}); 
